0b0012cd8f17cc13836111552a9c95a691781232
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-03-24 21:52:44 +0700

    Push Project Simple Todo List Vue Js

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row header">
+        <div class="col-lg-12">
+          <h2>What I do Today ?</h2>
+        </div>
+      </div>
+      <div class="row content">
+        <div class="col-lg-12">
+          <div class="pekerjaan-rumah">
+            <div class="row">
+              <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas col-lg-12 text-start mt-2">
+                <div class="form-check">
+                  <ItemTugas v-bind:tugasSaya="tugasSaya" />
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>  
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+
+import ItemTugas from "./components/ItemTugas.vue";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+    tugas: [
+      {
+        id: 1,
+        namaTugas: "Membersihkan Toilet",
+        isDone: false
+      },
+      {
+        id: 2,
+        namaTugas: "Makan Bakso",
+        isDone: false
+      },
+      {
+        id: 3,
+        namaTugas: "Gym Fit",
+        isDone: false
+      },
+      {
+        id: 4,
+        namaTugas: "Istirahat 8 Jam",
+        isDone: false
+      },
+    ]
+    };
+  },
+  components: {
+    ItemTugas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.header{
+  background: #1abc9c;
+}
+.header h2 {
+  margin: 20px;
+}
+
+.tugas{ 
+  margin-bottom: 30px;
+}
+
+.tugas .item{
+  font-size: 20px;
+}
+
+.item-completed {
+  color:#1abc9c;
+}
+</style>
---
+++ src/components/ItemTugas.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input class="form-check-input" onclick="" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label item" v-bind:class="{'item-completed':tugasSaya.isDone}" for="defaultCheck1">
+        {{ tugasSaya.namaTugas }}
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ItemTugas",
+    props: ["tugasSaya"]
+}
+</script>
+
+<style scoped>
+
+</style>
